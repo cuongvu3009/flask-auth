e4e98592ff5a93b1c12648ce94f22ecc3d29a123
cuongvu3009 <cuongvu3009@gmail.com>
2023-05-12 14:47:53 +0300

    flask-session

--- api/app.py
+++ api/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_session import Session
+
 from models import User, db
 from config import ApplicationConfig
 
@@ -7,10 +9,24 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    
+@app.route("/@me")
+def get_current_user():
+  user_id = session.get("user_id")
+  
+  if not user_id:
+    return jsonify({"error": "Unauthorized"}), 401
+  
+  user = User.query.filter_by(id=user_id).first()
+  return jsonify({
+    "id": user.id,
+    "email": user.email
+  })
     
 @app.route("/register", methods=['POST'])
 def register_user():
@@ -45,6 +61,8 @@ def login_user():
     
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
+      
+    session["user_id"] = user.id
     
     return jsonify({
         "id": user.id,
